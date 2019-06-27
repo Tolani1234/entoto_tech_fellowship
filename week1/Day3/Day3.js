ae95d655fd24c2c5d96e4bb7b53a68288c7a1f1e
Tolani1234 <naatoo2018@gmail.com>
2019-06-27 16:03:21 +0300

    first week

---
+++ week1/Day3/Day3.js
@@ -0,0 +1,12 @@
+var TempratureValue=20;
+
+if(TempratureValue < 10){
+    console.log("It's cold temprature")
+}else if (TempratureValue >= 10 && TempratureValue <= 20 ){
+
+    console.log("It's nice temprature")
+}
+else if(TempratureValue > 20)
+{
+    console.log("it's warm temprature")
+}
---
+++ week1/Day5/Day5.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <script>
+    
+        var for={(i=1, i>=99,  i++)
+       
+         (i % 3 === 0 && i % 5 ===0 )
+        }
+           {
+               console.log("FIZZBUZZ") 
+            }
+    else if(i % 3 === 0 ){
+        console.log("FIZZ")
+    }
+    else  if(i % 5 === 0 ){
+        console.log("BUZZ")
+    }
+   
+    else{
+        console.log(i)
+        }
+    </script>
+
+   </head>
+  <body>
+ </body>    
+</html>
